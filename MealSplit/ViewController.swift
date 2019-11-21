e7fe37d78651b57cf49d059f38f6f15d549e2ef1
Allen <54394182+PrintName@users.noreply.github.com>
2019-11-20 23:50:32 -0500

    Initial Commit

---
+++ MealSplit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MealSplit
+//
+//  Created by Allen Li on 11/20/19.
+//  Copyright © 2019 Allen Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
